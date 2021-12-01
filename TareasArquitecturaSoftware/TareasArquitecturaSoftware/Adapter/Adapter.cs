58112c6949e9a4c341da2d01da5c5355f0ff11f1
Nidia Ávila <66445181+Nidiax@users.noreply.github.com>
2021-11-30 19:23:40 -0600

    Add files via upload

---
+++ TareasArquitecturaSoftware/TareasArquitecturaSoftware/Adapter/Adapter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TareasArquitecturaSoftware.Adapter
+{
+    public class Adapter
+    {
+        public static void MainAdapter()
+        {            
+            Adaptee adaptee = new Adaptee();
+            ITarget target = new AdapteClass(adaptee);
+
+            Console.WriteLine("Adaptee interface is incompatible with the client.");
+            Console.WriteLine("But with adapter client can call it's method.");
+
+            Console.WriteLine(target.GetRequest());
+        }
+    }
+
+    public interface ITarget
+    {
+        string GetRequest();
+    }
+
+    // The Adaptee contains some useful behavior, but its interface is
+    // incompatible with the existing client code. The Adaptee needs some
+    // adaptation before the client code can use it.
+    class Adaptee
+    {
+        public string GetSpecificRequest()
+        {
+            return "Specific request.";
+        }
+    }
+
+    // The AdapterClass makes the Adaptee's interface compatible with the Target's
+    // interface.
+    class AdapteClass: ITarget
+    {
+        private readonly Adaptee _adaptee;
+
+        public AdapteClass(Adaptee adaptee)
+        {
+            this._adaptee = adaptee;
+        }
+
+        public string GetRequest()
+        {
+            return $"This is '{this._adaptee.GetSpecificRequest()}'";
+        }
+    }
+}
---
+++ TareasArquitecturaSoftware/TareasArquitecturaSoftware/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace TareasArquitecturaSoftware
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Builder.Builder.MainBuilder();
+            //Adapter.MainAdapter();
+            //Bridge.MainBridge();
+            //Composite.Composite.MainComposite();
+            //Observer.Observer.MainObserver();
+            //StateClass.StateClass.MainState();
+            Strategy.Strategy.MainStrategy();
+        }
+    }
+}
